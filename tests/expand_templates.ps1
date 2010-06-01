cf009f7063fd073166caa50da9c976a157592dd8
dglaser <dale.glaser@gmail.com>
2010-06-01 18:10:48 -0500

    changed paths to test files

--- tests/expand_templates.ps1
+++ tests/expand_templates.ps1
@@ -1,9 +1,9 @@
 
 . .\psake_ext.ps1
 
-[string]$temp_data = 'C:\workspace\Rex\EmployeeSite\trunk\templates\data\dev\template.data'
-[string]$temp_file = 'C:\workspace\Rex\EmployeeSite\trunk\templates\web.config.template'
-[string]$dest_file = 'C:\workspace\Rex\EmployeeSite\trunk\templates\web.config'
+[string]$temp_data = 'test.data'
+[string]$temp_file = 'test.template'
+[string]$dest_file = 'test.txt'
 
 expand_templates $temp_data $temp_file $dest_file $true
 
